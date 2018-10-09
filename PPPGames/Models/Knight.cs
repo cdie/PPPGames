ded66149e1e82e1bde1f002452b21cd4ceabf33b
Christophe MOMMER <chris.mommer@domaine-epsilog.local>
2018-10-09 14:35:49 +0200

    Ajoutez des fichiers projet.

---
+++ PPPGames/Models/Knight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPPGames.Models
+{
+    class Knight : Warrior
+    {
+        public override void Hit(Warrior other)
+        {
+            other.TakeHit(this);
+        }
+    }
+}
---
+++ PPPGames/Models/Warrior.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PPPGames.Models
+{
+    public abstract class Warrior
+    {
+        public string Name { get; set; }
+        public int PointsOfLife { get; set; }
+        public int SwordDamage { get; set; }
+        public bool Alive { get; set; }
+
+        public abstract void Hit(Warrior other);
+
+        public void TakeHit(Warrior other)
+        {
+            PointsOfLife = (int)Math.Max(0, PointsOfLife - other.SwordDamage);
+            Alive = PointsOfLife > 0;
+        }
+
+    }
+}
